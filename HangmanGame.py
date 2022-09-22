eb4e2eed88c98a7d09637f788ccdbb159818889a
Matt <98755798+m-inwards@users.noreply.github.com>
2022-09-22 18:44:40 +0100

    Game completed

--- HangmanGame.py
+++ HangmanGame.py
@@ -1,4 +1,5 @@
 from random import shuffle
+from nltk.corpus import words
 
 
 class HangmanGame:
@@ -18,8 +19,18 @@ class HangmanGame:
         # Start the game
         self.play_game()
 
+    def restart_game(self):
+        self.selected_word = ""
+        self.guessed_letters = []
+        self.num_of_guesses_left = 13
+        self.correct_answer_guessed = False
+        self.guess = ""
+
+        self.select_word()
+        self.play_game()
+
     def populate_dictionary(self):
-        self.dictionary = ["desk", "computer", "apple", "pear", "pineapple"]
+        self.dictionary = words.words()
 
     def select_word(self):
         self.shuffle_dictionary()
@@ -73,11 +84,23 @@ class HangmanGame:
             # Checks if they have got the word
             self.check_answer()
 
+        # Responds whether they got it or not and the appropriate message
         if self.correct_answer_guessed:
             self.display_hangman_characters()
             print("Congratulations, you got it right!")
         else:
             print(f"Commiserations, you didnt quite get it. The word was: {self.selected_word}")
+
+        # Checks if the player wants to play again
+        play_again = ""
+
+        while play_again.lower() not in ["yes", "y", "no", "n"]:
+            play_again = input("Would you like to play again? (Y/N):\n")
+
+        if play_again.lower() in ["yes", "y"]:
+            self.restart_game()
+        else:
+            print("Thanks for playing, I hope you had fun :)")
 
 
 if __name__ == "__main__":
